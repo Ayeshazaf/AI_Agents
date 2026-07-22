b6ddb58f8deff4255fa727bfa20d90c2046697a7
unknown <ayeshazafar5789@gmail.com>
2026-07-22 15:31:36 +0500

    all agents working

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from fastapi import FastAPI
 
 import shared.init_db
 from CRM_Agent.routes import router as crm_router
-from RAG_Agent.ingest import router as ingest_app
-from RAG_Agent.retriever import router as rag_router
 from Task_Agent.routes import router as task_router
 from Reporting_Agent.routes import router as reporting_router
 app = FastAPI()
 
 app.include_router(task_router)
-app.include_router(ingest_app)
-app.include_router(rag_router)
 app.include_router(crm_router)
 app.include_router(reporting_router)
